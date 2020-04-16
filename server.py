dc493024464c59d0a21c68929b7264d32ecdb5d3
Cheney Daniel <cheneydaniel8@gmail.com>
2020-04-15 23:15:50 -0400

    Added UID generator to POST function

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
-
 from io import BytesIO
+import uuid
+
 content = "gone fishin"
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
@@ -9,19 +10,18 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         globals_list = globals()
         self.send_response(200)
         self.end_headers()
-        print(globals_list)
         self.wfile.write(bytes(globals_list["content"], "utf-8"))
 
     def do_POST(self):
         global content
         content_length = int(self.headers['Content-Length'])
         request = self.rfile.read(content_length)
+        id = bytes(str(uuid.uuid4()), "utf-8")
         self.send_response(200)
         self.end_headers()
         response = BytesIO()
-        response.write(b'This is POST request. ')
-        response.write(b'Received: ')
-        response.write(request)
+        response.write(b'Your entry ID is: ')
+        response.write(id)
         content = str(request)
         self.wfile.write(response.getvalue())
 
