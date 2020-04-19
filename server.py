746aab09a903d1b5265eb669e1951af3844ca088
Cheney Daniel <cheneydaniel8@gmail.com>
2020-04-18 21:52:31 -0400

    working on connecting get request to mysql db

--- server.py
+++ server.py
@@ -9,12 +9,10 @@ from mysql.connector import Error
 from mysql.connector import errorcode
 
 
-entries_dict = dict()
-
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        # API endpoints
+
         if self.path.startswith("/api"):
 
             if self.path.endswith("/all-entries"):
@@ -23,8 +21,27 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
                 self.send_header("Access-Control-Allow-Origin", "*")
                 self.end_headers()
 
-                response = json.dumps(entries_dict)
-                self.wfile.write(response)
+                try:
+                    connection = mysql.connector.connect(host='localhost',
+                                                         database='devbros',
+                                                         user='root',
+                                                         password='!Five56five')
+                    cursor = connection.cursor(prepared=True)
+                    sql_insert_query = """SELECT * FROM posts;"""
+                    cursor.execute(sql_insert_query)
+                    response = cursor.fetchall()
+                    connection.commit()
+                    cursor.close()
+
+                except mysql.connector.Error as error:
+                    print("Failed to access records".format(error))
+
+                finally:
+                    if (connection.is_connected()):
+                        connection.close()
+                        print("MySQL connection is closed")
+
+                self.wfile.write(json.dumps(response).encode())
 
             if ("/entry") in self.path:
                 self.send_response(200)
@@ -52,40 +69,9 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
                 response = json.dumps()
                 self.wfile.write(response)
-        # HTML router
-        else:
-            if self.path.endswith("/entries"):
-                self.send_response(200)
-                self.send_header("content-type", "text/html")
-                self.end_headers()
-
-                output = ""
-                output += "<html><body>"
-                output += "<h1>Entry List</h1>"
-                output += "<h3><a href='/entries/new'>Add New Entry</a></h3>"
-                for entry in entries_list:
-                    output += entry
-                    output += "</br>"
-                output += "</body></html>"
-                self.wfile.write(output.encode())
-
-            if self.path.endswith("/new"):
-                self.send_response(200)
-                self.send_header("content-type", "text/html")
-                self.end_headers()
-
-                output = ""
-                output += "<html><body>"
-                output += "<h1>Add New Entry</h1>"
-                output += "<form method='POST' enctype='multipart/form-data' action='/entries/new'>"
-                output += "<input name='entry' type='text' placeholder='Add new entry'>"
-                output += "<input type='submit' value='Add'>"
-                output += "</form>"
-                output += "</body></html>"
-                self.wfile.write(output.encode())
 
     def do_POST(self):
-        # API endpoints
+
         if self.path.startswith("/api"):
             # Endpoint for adding single entry
             if self.path.endswith("/new-entry"):
@@ -124,28 +110,11 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
                         connection.close()
                         print("MySQL connection is closed")
 
-
                 response = json.dumps(new_ID).encode()
                 self.wfile.write(response)
-        # HTML router
-        else:
-            if self.path.endswith("/new"):
-                ctype, pdict = cgi.parse_header(self.headers.get("content-type"))
-                pdict['boundary'] = bytes(pdict['boundary'], "utf-8")
-                content_len = int(self.headers.get('Content-length'))
-                pdict['CONTENT-LENGTH'] = content_len
-                if ctype == "multipart/form-data":
-                    fields = cgi.parse_multipart(self.rfile, pdict)
-                    new_entry = fields.get("entry")
-                    entries_list.append(new_entry[0])
-
-                    self.send_response(301)
-                    self.send_header("content-type", "text/html")
-                    self.send_header("Location", "/entries")
-                    self.end_headers()
 
     def do_DELETE(self):
-        # API endpoints
+
         if self.path.startswith("/api"):
             if ("/entry") in self.path:
                 self.send_response(200)
